245b6ee6509b11aac88dde1dd4750be96ad31f9b
Shravan Vemula <shravan17298@gmail.com>
2020-03-20 12:03:42 +0530

    Employee management webapp using thymeleaf and spring boot

---
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,63 @@
+package com.luv2code.springboot.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository theEmployeeRepo) {
+		employeeRepository = theEmployeeRepo;
+	}
+	
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee emp=null;
+		if(result.isPresent()) {
+			emp=result.get();
+		}
+		else {
+			throw new RuntimeException("Did not find employee id "+theId);
+		}
+		return emp;
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		employeeRepository.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeRepository.deleteById(theId);
+	}
+
+}
+
+
+
+
+
+
