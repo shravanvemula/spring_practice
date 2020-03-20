085798072f98d2eb37b811397dd54afd6c4efb88
Shravan Vemula <shravan17298@gmail.com>
2020-03-20 13:00:40 +0530

    autowiring-assignment-springboot

---
+++ scorecardApplication/src/main/java/com/assignment/cricket/player/IFielder.java
@@ -0,0 +1,10 @@
+package com.assignment.cricket.player;
+
+public interface IFielder {
+	
+	public void setNumberOfCatches(int catches);
+	public void setNumberOfRunsStopped(int runs);
+	public int getNumberOfCatches();
+	public int getNumberOfRunsStopped();
+
+}
